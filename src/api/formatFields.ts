efa4a6a78fac173d5dd3ea4a266e1459900b9ed0
Michał Grudzień <michal.grudzien1@gmail.com>
2021-12-23 19:33:54 +0100

    Fix date formatting

--- src/api/formatFields.ts
+++ src/api/formatFields.ts
@@ -1,6 +1,9 @@
+import { formatISO } from "date-fns";
+
 export default (fields: any) => ({
   transportationOutbound: "",
   transportationReturn: "",
   ...fields,
+  dateBirth: fields.dateBirth? formatISO(fields.dateBirth, { representation: 'date' }) : undefined,
   sailingExperience: parseInt(fields.sailingExperience),
 });
